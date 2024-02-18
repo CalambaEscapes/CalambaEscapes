25e84dfaadce206b4a0f705a5a582a6e9c9bbbc6
JohnKyleMicosa <160302577+JohnKyleMicosa@users.noreply.github.com>
2024-02-18 16:59:01 +0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,3 @@
+function aboutUs(){
+    window.alert("This webpage was created by our team. John Kyle Micosa,\n Arnold Jay Quintin, Aljohn Paul Villarin, Ella Nace and\n Loren Gebrielle Fajardo. We would like to thank you for\n visiting our webpage and we hope that you gain knowledge\n about our beloved City of Calamba.");
+}
